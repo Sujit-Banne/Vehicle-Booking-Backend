472628bf15195948543362a176ccd44ce7bbc9f3
Sujit Banne <sujitbanne13@gmail.com>
2023-05-09 13:28:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+require('dotenv').config()
+const MONGO_URI = process.env.MONGO_URI
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors())
+
+//connection to db
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        console.log('connected to db');
+        app.listen(PORT, () => {
+            console.log(`server is running on port ${PORT}`);
+        })
+    }).catch(err => {
+        console.log(err);
+    })
